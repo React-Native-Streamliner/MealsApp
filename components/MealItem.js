6bc66d340777284997efae4f287d5196da0728ad
tiger <thisisstreamliner@gmail.com>
2023-09-16 23:33:40 +0300

    Add MealItem Styles

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,7 +1,7 @@
 import { View, Text, Pressable, Image, StyleSheet } from "react-native"
 
 function MealItem({ title, imageUrl, duration, complexity, affordability }) {
-    return <View>
+    return <View style={styles.mealItem}>
         <Pressable>
             <View>
                 <Image 
@@ -10,10 +10,10 @@ function MealItem({ title, imageUrl, duration, complexity, affordability }) {
                     onError={(error) => console.error(error.nativeEvent.error)} />
                 <Text style={styles.title} >{title}</Text>
             </View>
-            <View>
-                <Text>{duration}m</Text>
-                <Text>{complexity.toUpperCase()}</Text>
-                <Text>{affordability.toUpperCase()}</Text>
+            <View style={styles.details}>
+                <Text style={styles.detailItem}>{duration}m</Text>
+                <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
+                <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
             </View>
         </Pressable>
     </View>
@@ -22,6 +22,12 @@ function MealItem({ title, imageUrl, duration, complexity, affordability }) {
 export default MealItem
 
 const styles = StyleSheet.create({
+    mealItem: {
+        margin: 16,
+        borderRadius: 8,
+        overflow: 'hidden',
+        backgroundColor: 'white',
+    },
     image: {
         width: '100%',
         height: 200,
@@ -29,6 +35,17 @@ const styles = StyleSheet.create({
     title: {
         fontWeight: 'bold',
         textAlign: 'center',
-        fontSize: 18
+        fontSize: 18,
+        margin: 8,
+    },
+    details: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 8,
+    },
+    detailItem: {
+        marginHorizontal: 4,
+        fontSize: 12
     }
 })
